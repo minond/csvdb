2c739189990cbc47ec18a0b98e30d9f0911bc744
Marcos Minond <minond.marcos@gmail.com>
2017-10-28 21:48:48 -0600

    auto test runner

--- src/test/csvdb/Test.java
+++ src/test/csvdb/Test.java
@@ -1,38 +1,67 @@
 package csvdb;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
-  protected Parser parser = new Parser();
-
-  public static void main(String[] args) {
-    Test test = new Test();
-
-    test.testSimpleList();
-    test.testSpacedElements();
-    test.testEscapedCharacters();
-    test.testDelimeterInQuotes();
-
+  public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    System.out.println("Running tests");
+    (new ParserTests()).runTests();
     System.out.println("Done, all tests passed!");
   }
 
-  public void testSimpleList() {
-    assert Arrays.asList("one", "two", "three")
-      .equals(parser.parseLine("one,two,three"));
+  static class Tester {
+    public void runTests() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+      String klass = this.getClass().getName();
+
+      for (String method : this.getTests()) {
+        System.out.printf("Running %s.%s... ", klass, method);
+        Method m = this.getClass().getMethod(method);
+        System.out.printf("OK\n");
+        m.invoke(this);
+      }
+    }
+
+    public List<String> getTests() {
+      List<String> tests = new ArrayList<String>();
+
+      Class klass = this.getClass();
+      Method[] methods = klass.getDeclaredMethods();
+
+      for (int i = 0; i < methods.length; i++) {
+        if (methods[i].getName().startsWith("test")) {
+          tests.add(methods[i].getName());
+        }
+      }
+
+      return tests;
+    }
   }
 
-  public void testSpacedElements() {
-    assert Arrays.asList("one", "two", "three")
-      .equals(parser.parseLine("   one   ,   two  ,  three  "));
-  }
+  static class ParserTests extends Tester {
+    protected Parser parser = new Parser();
 
-  public void testEscapedCharacters() {
-    assert Arrays.asList("one", "two", "three")
-      .equals(parser.parseLine("  o\\ne         ,  \\two ,    thre\\e  "));
-  }
+    public void testSimpleList() {
+      assert Arrays.asList("one", "two", "three")
+        .equals(parser.parseLine("one,two,three"));
+    }
 
-  public void testDelimeterInQuotes() {
-    assert Arrays.asList("one", "tw,o", "three", ",,,", "four")
-      .equals(parser.parseLine("one, \"tw,o\",three,\",,,\",four"));
+    public void testSpacedElements() {
+      assert Arrays.asList("one", "two", "three")
+        .equals(parser.parseLine("   one   ,   two  ,  three  "));
+    }
+
+    public void testEscapedCharacters() {
+      assert Arrays.asList("one", "two", "three")
+        .equals(parser.parseLine("  o\\ne         ,  \\two ,    thre\\e  "));
+    }
+
+    public void testDelimeterInQuotes() {
+      assert Arrays.asList("one", "tw,o", "three", ",,,", "four")
+        .equals(parser.parseLine("one, \"tw,o\",three,\",,,\",four"));
+    }
   }
 }
